f81d83a6b57e3eafb0252a9fa063f02cbb9e8501
supremebeing7 <markopolo@gmail.com>
2015-11-10 00:24:14 -0800

    complete order specs

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,45 +2,45 @@ require 'spec_helper'
 
 describe Order do
   describe '#initializing' do
-    it 'should initialize if source is valid' do
+    it 'should work fine if source is valid' do
       expect { Order.new(quantity: 10, source_name: "ACompany") }.not_to raise_error
     end
-    it 'should raise error if source is invalid' do
+    it 'raises an error if source is invalid' do
       expect { Order.new(quantity: 10, source_name: "invalid") }.to raise_error InvalidSource
     end
-    it 'should total amount paid from source price times quantity' do
+    it 'sets the total amount paid equal to source price times quantity' do
       source = instance_double('Source', name: 'test', price: 10)
       allow(Source).to receive(:find_by_name).and_return(source)
       order = Order.new(quantity: 10)
       expect(order.amount_paid).to eq 100
     end
   end
-  # def initialize(attributes)
-  #   raise InvalidSource unless source = Source.find_by_name(attributes[:source_name])
-  #   @quantity = attributes[:quantity]
-  #   @source_name = source.name
-  #   @amount_paid = source.price * quantity
-  # end
 
-  # def self.last_month
-  #   # This will change when dates are added, but checking dates was not in the
-  #   # scope of the project description
-  #   generate_random(100)
-  # end
+  describe '.last_month' do
+    # This statement is not actually accurate in the current implementation
+    # but it is what this method would do if we were not working with random data
+    it 'retrieves orders from the last month' do
+      expect(Order).to receive(:generate_random).with(100)
+      Order.last_month
+    end
+  end
 
-  # def self.generate_random(number)
-  #   orders = []
-  #   number.times do
-  #     orders << new_random_order
-  #   end
-  #   orders
-  # end
+  describe '.generate_random' do
+    it 'builds new random orders' do
+      expect(Order).to receive(:new_random_order).exactly(42).times
+      Order.generate_random(42)
+    end
+    it 'generates as many orders as requested' do
+      expect(Order.generate_random(100).size).to eq 100
+      expect(Order.generate_random(42).size).to eq 42
+    end
+  end
 
-  # def self.new_random_order
-  #   quantity = rand(100)
-  #   source = Source.all.sample
-  #   amount_paid = source.price * quantity
-  #   new(quantity: quantity, source_name: source.name, amount_paid: amount_paid)
-  # end
-
+  describe '.new_random_order' do
+    it 'builds a new order with random sample data' do
+      order = Order.new_random_order
+      expect(order.quantity).to be_between(1,100)
+      expect(Source.names).to include order.source_name
+    end
+  end
 end
--- spec/report_spec.rb
+++ spec/report_spec.rb
@@ -1,5 +1,46 @@
 require 'spec_helper'
 
 describe Report do
-  
+  # describe '#initializing' do
+  #   it 'should work fine if source is valid' do
+  #     expect { Order.new(quantity: 10, source_name: "ACompany") }.not_to raise_error
+  #   end
+  #   it 'raises an error if source is invalid' do
+  #     expect { Order.new(quantity: 10, source_name: "invalid") }.to raise_error InvalidSource
+  #   end
+  #   it 'sets the total amount paid equal to source price times quantity' do
+  #     source = instance_double('Source', name: 'test', price: 10)
+  #     allow(Source).to receive(:find_by_name).and_return(source)
+  #     order = Order.new(quantity: 10)
+  #     expect(order.amount_paid).to eq 100
+  #   end
+  # end
+
+  # describe '.last_month' do
+  #   # This statement is not actually accurate in the current implementation
+  #   # but it is what this method would do if we were not working with random data
+  #   it 'retrieves orders from the last month' do
+  #     expect(Order).to receive(:generate_random).with(100)
+  #     Order.last_month
+  #   end
+  # end
+
+  # describe '.generate_random' do
+  #   it 'builds new random orders' do
+  #     expect(Order).to receive(:new_random_order).exactly(42).times
+  #     Order.generate_random(42)
+  #   end
+  #   it 'generates as many orders as requested' do
+  #     expect(Order.generate_random(100).size).to eq 100
+  #     expect(Order.generate_random(42).size).to eq 42
+  #   end
+  # end
+
+  # describe '.new_random_order' do
+  #   it 'builds a new order with random sample data' do
+  #     order = Order.new_random_order
+  #     expect(order.quantity).to be_between(1,100)
+  #     expect(Source.names).to include order.source_name
+  #   end
+  # end
 end
